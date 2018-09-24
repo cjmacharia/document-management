5ea306ab4952549118a94aebc6b8498beb598468
cjmash <james.macharia@andela.com>
2018-09-24 13:14:40 +0300

    ft (signup): update the user sign up functionality

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,21 +1,19 @@
 const  User = require('../models/user-model');
 
 exports.signUp = (req, res) => {
-	const user = new User({
+	const userSignUp = new User({
 		name: req.body.name,
 		email: req.body.email,
 		password: req.body.password
 	});
-	user.save()
+	userSignUp.save()
 		.then(data => {
-			console.log('>><><><><><><');
 			console.log(data);
 			res.status(200);
 			res.json({
 				message: 'Succssfully created a user',
 				response: data});
 		}).catch(err => {
-			console.log('>><><><><><><');
 			res.status(500).json({
 				message: err.message
 			});
